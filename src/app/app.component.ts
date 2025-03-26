10072d21be9c010ab52bf28228b466666f8ce498
vbotelhog <vbotelhog@minsait.com>
2025-03-26 12:51:35 -0300

    Configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    1<router-outlet></router-outlet>2`
 })
 export class AppComponent {
   title = 'list-task';
