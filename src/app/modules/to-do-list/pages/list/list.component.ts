362b515e3b55b83769c9f6b8b75096a1ccb5662a
vbotelhog <vbotelhog@minsait.com>
2025-04-07 17:14:30 -0300

    Lista de itens

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,12 +1,33 @@
 import { Component, signal } from '@angular/core';
+import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
+import { IListItems } from '../../interface/IlistItems.interface';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [InputAddItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
 
   public addItem = signal(true);
+
+  #setListItems = signal<IListItems[]>(this.#parseListItems());
+  public getListItems = this.#setListItems.asReadonly();
+
+  #parseListItems() {
+    return JSON.parse(localStorage.getItem('@my-list') || '[]');
+  }
+  public getIputAndAddItem(value: IListItems) {
+    localStorage.setItem(
+      '@my-list', JSON.stringify([...this.#setListItems() ,value])
+    )
+
+    return this.#setListItems.set(this.#parseListItems());
+  }
+
+  public deleteAllItems() {
+    localStorage.removeItem('@my-list');
+    return this.#setListItems.set(this.#parseListItems());
+  }
 }
